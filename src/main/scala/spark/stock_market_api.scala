e84f30362b2ff861dda8b04ca3fc63c388b573df
Sujaymc <smc86uk@gmail.com>
2025-01-06 13:59:07 +0000

    First

--- src/main/scala/spark/stock_market_api.scala
+++ src/main/scala/spark/stock_market_api.scala
@@ -3,6 +3,7 @@ package spark
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.functions._
 import org.apache.spark.sql.types._
+import requests._
 
 object stock_market_api {
 
